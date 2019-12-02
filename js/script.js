628cec14367c70843ab5f8816376fc30f140bb8e
AndrewHuntington <andrew.huntington@gmail.com>
2019-12-02 16:19:55 +0900

    create 16x16 div grid using javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert('Hi!');
+const etchSketch = document.querySelector('.etch-a-sketch');
+let easDiv;
+
+for (var i = 1; i <= 16; i++) {
+  for (var j = 1; j <= 16; j++) {
+    easDiv = document.createElement('div');
+    easDiv.setAttribute('id', `${i}-${j}`);
+    easDiv.style.backgroundColor = "blue";
+    easDiv.textContent = `${i}-${j}`;
+    etchSketch.appendChild(easDiv);
+  }
+}
