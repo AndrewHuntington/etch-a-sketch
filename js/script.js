151e448e3319edd14d3cc4969e2565f36dabab13
AndrewHuntington <andrew.huntington@gmail.com>
2019-12-02 22:56:18 +0900

    all basic functionality is present

--- js/script.js
+++ js/script.js
@@ -1,30 +1,70 @@
 const etchSketchCanvas = document.querySelector('.etch-a-sketch');
 let easDiv;
 
-for (var i = 1; i <= 16; i++) {
-  for (var j = 1; j <= 16; j++) {
-    easDiv = document.createElement('div');
-    easDiv.setAttribute('id', `${i}-${j}`);
-    // easDiv.textContent = `${i}-${j}`;
-    etchSketchCanvas.appendChild(easDiv);
+function createGrid(width, height){
+  // error handling -- set default values if prompt is blank or 0
+  if (!width) {
+    width = 16;
+  }
+  if (!height) {
+    height = 16;
+  }
+
+  etchSketchCanvas.style.gridTemplateColumns = `repeat(${width}, ${640 / width}px)`;
+  etchSketchCanvas.style.gridTemplateRows = `repeat(${height}, ${640 / height}px)`;
+  for (var i = 1; i <= height; i++) {
+    for (var j = 1; j <= width; j++) {
+      easDiv = document.createElement('div');
+      easDiv.setAttribute('id', `${i}-${j}`);
+      // easDiv.textContent = `${i}-${j}`;
+      etchSketchCanvas.appendChild(easDiv);
+    }
   }
 }
 
-const canvasDivs = document.querySelectorAll(".etch-a-sketch > div");
-canvasDivs.forEach((canvasDiv) => {
-  canvasDiv.addEventListener('mouseover', () => {
-    canvasDiv.classList.add('black');
-  });
-});
+function removeGrid() {
+  // removes all divs under .etch-a-sketch
+  while (etchSketchCanvas.firstChild) {
+    etchSketchCanvas.removeChild(etchSketchCanvas.firstChild);
+  }
+}
 
-const btn = document.querySelector("#reset");
-btn.addEventListener('click', (e) => {
+function setNewGrid() {
+  removeGrid();
+  const width = +prompt("Please enter the number of grids for the width:");
+  const height = +prompt("Please enter the number of grids for the height:");
+  createGrid(width, height);
+  setClassBlack();
+}
+
+function setClassBlack() {
+  // changes the boxes' color to black when moused over
+  const canvasDivs = document.querySelectorAll(".etch-a-sketch > div");
+  canvasDivs.forEach((canvasDiv) => {
+    canvasDiv.addEventListener('mouseover', () => {
+      canvasDiv.classList.add('black');
+    });
+  });
+}
+
+function unsetClassBlack() {
+  // resets all boxes back to default color
+  const canvasDivs = document.querySelectorAll(".etch-a-sketch > div");
+  canvasDivs.forEach((canvasDiv) => {
+    canvasDiv.classList.remove('black');
+  });
+}
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener('click', () => {
   let result = window.confirm('Clear the canvas?');
   if (result) {
-    canvasDivs.forEach((canvasDiv) => {
-      canvasDiv.classList.remove('black');
-    });
+    unsetClassBlack();
+    setNewGrid();
   } else {
     return;
   }
 });
+
+createGrid(16, 16);
+setClassBlack();
